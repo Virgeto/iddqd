b7a9a43a3d5be3ff8c5e7bed4bdd7c4651da8e29
Vassil Dimitrov <virgeto@gmail.com>
2015-03-15 19:39:46 +0200

    Status 100% home page, contacts ans 10% Western union

--- js/routes/angular-routes.js
+++ js/routes/angular-routes.js
@@ -1,5 +1,8 @@
 currencyApp.config(function($routeProvider){
                                 $routeProvider
-                                    .when('/home',{templateUrl: 'partials/under_construction.html', controller: 'currencyController'})
+                                    .when('/home',{templateUrl: 'partials/home.html', controller: 'currencyController'})
+                                    .when('/quotes',{templateUrl: 'partials/under_construction.html', controller: 'currencyController'})
+                                    .when('/contact',{templateUrl: 'partials/contact.html', controller: 'currencyController'})
+                                    .when('/western',{templateUrl: 'partials/western_union.html', controller: 'currencyController'})
                                     .otherwise({redirectTo : '/home', templateUrl: 'partials/home.html', controller:'currencyController'});
                                 })
