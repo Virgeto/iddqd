69d5149a388311345cb505d0f66f5432f2fcd268
Vassil Dimitrov <virgeto@gmail.com>
2015-03-15 12:39:39 +0200

    Status 15% home page

--- js/routes/angular-routes.js
+++ js/routes/angular-routes.js
@@ -1,5 +1,5 @@
 currencyApp.config(function($routeProvider){
                                 $routeProvider
-                                    .when('/home',{templateUrl: 'partials/home.html', controller: 'currencyController'})
+                                    .when('/home',{templateUrl: 'partials/under_construction.html', controller: 'currencyController'})
                                     .otherwise({redirectTo : '/home', templateUrl: 'partials/home.html', controller:'currencyController'});
                                 })
