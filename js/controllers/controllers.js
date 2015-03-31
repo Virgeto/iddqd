5f9df561e49a447fe468d6d46014c061fc5da7c5
Vassil Dimitrov <virgeto@gmail.com>
2015-03-31 14:15:45 +0300

    90% DONE

--- js/controllers/controllers.js
+++ js/controllers/controllers.js
@@ -2,14 +2,19 @@
 (function(){
 	"use strict"
 
-currencyApp.controller('currencyController' , function($scope, $location, $http, $interval, $routeParams){
-	
+currencyApp.controller('currencyController' , function($scope, $location, $http, $interval, $routeParams, $anchorScroll){
+	  $scope.scrollTo = function(id) {
+      $location.hash(id);
+      $anchorScroll();
+   }
+});
+currencyApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
+  //when the route is changed scroll to the proper element.
+  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
+	    $location.hash($routeParams.scrollTo);
+	    $anchorScroll();  
+  });
 });
 })();
 
-currencyApp.run(function($rootScope, $location, $anchorScroll, $routeParams) {
-  $rootScope.$on('$routeChangeSuccess', function(newRoute, oldRoute) {
-    $location.hash($routeParams.scrollTo);
-    $anchorScroll();  
-  });
-})
+
--- js/directives/buttons.js
+++ js/directives/buttons.js
@@ -33,3 +33,5 @@ currencyApp.directive('validateEmail', function() {
     }
   };
 });
+
+
